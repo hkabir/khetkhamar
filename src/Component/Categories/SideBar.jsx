7c51311c61cf0f54109c27a9f0e8ee262680f59a
khokon9363 <khokon9363@gmail.com>
2021-09-03 10:17:57 +0600

    Hridoy

--- src/Component/Categories/SideBar.jsx
+++ src/Component/Categories/SideBar.jsx
@@ -5,32 +5,48 @@ import { useGlobalContext } from "../../reducer/cartContext";
 
 export const SideBar = () => {
   const { category, caItem } = useGlobalContext();
-
+  const params = new URLSearchParams(window.location.search).toString()
   return (
     <div className="col-sm-3 flex-grow-sm-1 flex-shrink-1 flex-grow-0  pb-sm-0 pb-3">
       <div className="bg-light border rounded-3 p-1 h-100 ">
         <h6 className="d-none d-sm-block text-muted">Categories</h6>
         <ul className="nav nav-pills flex-sm-column flex-row mb-auto justify-content-between text-truncate">
           {category.map((item) => {
-            return (
-              <li className="nav-item" key={item.id}>
-                <a
-                  className="nav-link px-2 text-truncate"
-                  onClick={() => caItem(item.id)}
-                >
-                  <img
-                    src={`https://khetkhamar.org/public/${
-                      item.banner_img ? item.banner_img.file_name : ""
-                    }`}
-                    alt=""
-                    className="category-icon"
-                  />
-                  <span className="d-none d-sm-inline category-link">
-                    {item.name}
-                  </span>
-                </a>
-              </li>
-            );
+            console.log('Rendered')
+            if(item.parent_id == 0){
+              return (
+                <li className="nav-item" key={item.id} data-toggle="collapse" data-target={`#submenu${item.id}`}>
+                  <a
+                    className="nav-link px-2 text-truncate"
+                    onClick={() => caItem(item.id)}
+                  >
+                    <img
+                      src={`https://khetkhamar.org/public/${
+                        item.banner_img ? item.banner_img.file_name : ""
+                      }`}
+                      className="category-icon"
+                    />
+                    <span className="d-none d-sm-inline category-link">
+                      {item.name}
+                    </span>
+                  </a>
+                  { (item.children_categories.length > 0) &&
+                    <ul id={`submenu${item.id}`} className={`collapse ${params.split('=')[1] == item.id ? 'show' : ''}`}>
+                      {
+                        item.children_categories.map(item => (
+                          <li className="nav-item">
+                            <a 
+                              className="nav-link px-2 text-truncate"
+                              onClick={() => caItem(item.id)}
+                            >{item.name}</a>
+                          </li>
+                        ))
+                      }
+                    </ul>
+                  }
+                </li>
+              )
+            }
           })}
         </ul>
       </div>
